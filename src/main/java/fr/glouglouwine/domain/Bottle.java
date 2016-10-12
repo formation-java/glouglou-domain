a1b63a98e698f5bf7cead26851ec97aac5e9f040
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-10-12 20:38:56 +0200

    Add bottle constructor

--- src/main/java/fr/glouglouwine/domain/Bottle.java
+++ src/main/java/fr/glouglouwine/domain/Bottle.java
@@ -3,74 +3,86 @@ package fr.glouglouwine.domain;
 import java.time.LocalDateTime;
 
 public class Bottle {
-	
-	public Bottle(long bottleId, String owner, GrapeTypes grapeType, String domain, String year) {
-		super();
-		this.bottleId = bottleId;
-		this.owner = owner;
-		this.grapeType = grapeType;
-		this.domain = domain;
-		this.year = year;
-		this.quantity = 100;
-	}
-	
-	private long bottleId;
-	private String owner;
-	private GrapeTypes grapeType;
-	private String domain;
-	private String year;
-	private int quantity;
-	private LocalDateTime openingDate;
-	
-	public long getBottleId() {
-		return bottleId;
-	}
 
-	public String getOwner() {
-		return owner;
-	}
+    public Bottle(long bottleId, String owner, GrapeTypes grapeType, String domain, String year) {
+        this.bottleId = bottleId;
+        this.owner = owner;
+        this.grapeType = grapeType;
+        this.domain = domain;
+        this.year = year;
+        this.quantity = 100;
+    }
 
-	public GrapeTypes getGrapeType() {
-		return grapeType;
-	}
+    public Bottle(long bottleId, String owner, GrapeTypes grapeType, String domain, String year, int quantity,
+                  LocalDateTime openingDate, LocalDateTime finishingDate) {
+        this.bottleId = bottleId;
+        this.owner = owner;
+        this.grapeType = grapeType;
+        this.domain = domain;
+        this.year = year;
+        this.quantity = quantity;
+        this.openingDate = openingDate;
+        this.finishingDate = finishingDate;
+    }
 
-	public String getDomain() {
-		return domain;
-	}
+    private long bottleId;
+    private String owner;
+    private GrapeTypes grapeType;
+    private String domain;
+    private String year;
+    private int quantity;
+    private LocalDateTime openingDate;
 
-	public String getYear() {
-		return year;
-	}
+    public long getBottleId() {
+        return bottleId;
+    }
 
-	public double getQuantity() {
-		return quantity;
-	}
+    public String getOwner() {
+        return owner;
+    }
 
-	public LocalDateTime getOpeningDate() {
-		return openingDate;
-	}
+    public GrapeTypes getGrapeType() {
+        return grapeType;
+    }
 
-	public LocalDateTime getFinishingDate() {
-		return finishingDate;
-	}
-	private LocalDateTime finishingDate;
-	
-	public void drink(int quantity,LocalDateTime dateTime){
-		if(quantity<0){
-			throw new IllegalArgumentException("You can not drink a negative quantity of the bottle. Maybe you mean you refilled it");
-		}
-		if(this.quantity-quantity<0){
-			throw new IllegalArgumentException("You can not drink more than 100% of the bottle");
-		}
-		if(openingDate==null){
-			openingDate=dateTime;
-		}else if(openingDate.isAfter(dateTime)){
-			throw new IllegalArgumentException("You can not have drinked your bottle before his opening");
-		}
-		this.quantity=this.quantity-quantity;
-		if(this.quantity==0){
-			finishingDate=dateTime;
-		}
-	}
-	
+    public String getDomain() {
+        return domain;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public LocalDateTime getOpeningDate() {
+        return openingDate;
+    }
+
+    public LocalDateTime getFinishingDate() {
+        return finishingDate;
+    }
+
+    private LocalDateTime finishingDate;
+
+    public void drink(int quantity, LocalDateTime dateTime) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("You can not drink a negative quantity of the bottle. Maybe you mean you refilled it");
+        }
+        if (this.quantity - quantity < 0) {
+            throw new IllegalArgumentException("You can not drink more than 100% of the bottle");
+        }
+        if (openingDate == null) {
+            openingDate = dateTime;
+        } else if (openingDate.isAfter(dateTime)) {
+            throw new IllegalArgumentException("You can not have drinked your bottle before his opening");
+        }
+        this.quantity = this.quantity - quantity;
+        if (this.quantity == 0) {
+            finishingDate = dateTime;
+        }
+    }
+
 }
