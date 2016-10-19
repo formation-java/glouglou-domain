669dcf09a205271516284aab3ca5b9264e2a6a36
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-10-19 10:14:09 +0200

    test excercice

--- src/test/java/fr/glouglouwine/domain/BottleTest.java
+++ src/test/java/fr/glouglouwine/domain/BottleTest.java
@@ -1,8 +1,5 @@
 package fr.glouglouwine.domain;
 
-import java.time.LocalDateTime;
-
-import org.junit.Assert;
 import org.junit.Test;
 
 public class BottleTest {
@@ -11,70 +8,48 @@ public class BottleTest {
 	public void testBottle() throws Exception {
 		//Setup
 		Bottle bottle = new Bottle(1, "A", GrapeTypes.CABERNET, "domain","year");
-		//Test
-		Assert.assertEquals(100, bottle.getQuantity(),0);
+		// TODO : test quantity value
 	}
 	
 	@Test
 	public void testDrinkConsumein3times() throws Exception {
 		//Setup
 		Bottle bottle = new Bottle(1, "A", GrapeTypes.CABERNET, "domain","year");
-		
-		LocalDateTime drinkOpenDate = LocalDateTime.now().minusHours(2);
-		bottle.drink(20, drinkOpenDate);
-		//Assert
-		Assert.assertEquals(null, bottle.getFinishingDate());
-		Assert.assertEquals(drinkOpenDate, bottle.getOpeningDate());
-		Assert.assertEquals(80, bottle.getQuantity(),0);
-		
-		bottle.drink(20, LocalDateTime.now().minusHours(1));
-		//Assert
-		Assert.assertEquals(null, bottle.getFinishingDate());
-		Assert.assertEquals(drinkOpenDate, bottle.getOpeningDate());
-		Assert.assertEquals(60, bottle.getQuantity(),0);
-		
-		LocalDateTime drinkCloseDate = LocalDateTime.now().minusHours(2);
-		bottle.drink(60, drinkOpenDate);
-		//Assert
-		Assert.assertEquals(drinkCloseDate, bottle.getFinishingDate());
-		Assert.assertEquals(drinkOpenDate, bottle.getOpeningDate());
-		Assert.assertEquals(0, bottle.getQuantity(),0);
+
+		// TODO : drink the bottle in 3 times, test quantity, opening & finishing dates
 	}
 	
 	@Test
 	public void testDrinkConsumeAllInOneTime() throws Exception {
 		//Setup
 		Bottle bottle = new Bottle(1, "A", GrapeTypes.CABERNET, "domain","year");
-		LocalDateTime drinkDate = LocalDateTime.now().minusHours(2);
-		bottle.drink(100, drinkDate);
-		//Assert
-		Assert.assertEquals(drinkDate, bottle.getFinishingDate());
-		Assert.assertEquals(drinkDate, bottle.getOpeningDate());
-		Assert.assertEquals(bottle.getOpeningDate(), bottle.getFinishingDate());
-		Assert.assertEquals(0, bottle.getQuantity(),0);
+
+		// TODO : drink the whole bottle in one time
 	}
 	
-	@Test(expected=IllegalArgumentException.class)
+	@Test
 	public void testDrinkConsumeToMuch() throws Exception {
 		//Setup
 		Bottle bottle = new Bottle(1, "A", GrapeTypes.CABERNET, "domain","year");
-		bottle.drink(200, LocalDateTime.now());
+
+		// TODO : drink more than a bottle with one bottle...
+        // use @Test(expected=IllegalArgumentException.class)
 	}
 	
-	@Test(expected=IllegalArgumentException.class)
+	@Test
 	public void testDrinkConsumeNegative() throws Exception {
 		//Setup
 		Bottle bottle = new Bottle(1, "A", GrapeTypes.CABERNET, "domain","year");
-		bottle.drink(-1, LocalDateTime.now());
+
+		// TODO : try to drink a negative quantity
 	}
 	
-	@Test(expected=IllegalArgumentException.class)
+	@Test
 	public void testDrinkBeforeOpening() throws Exception {
 		//Setup
 		Bottle bottle = new Bottle(1, "A", GrapeTypes.CABERNET, "domain","year");
-		bottle.drink(50, LocalDateTime.now());
-		//Test
-		bottle.drink(20, LocalDateTime.now().minusMonths(1));
+
+		// TODO : drink a first time, and try to drink a second time with a date which is before the opening
 	}
 
 }
